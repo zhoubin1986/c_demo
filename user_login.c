121870ce8374afe964e03a2a015a2489cba80afc
zhoubin1986 <985619620@qq.com>
2021-01-22 00:52:57 -0800

    20210122upload

---
+++ user_login.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+void user_login()
+{
+	int i;
+	char password[8];
+	for(i=0;i<3 && strcmp(password,"abcd1111");i++)
+	{
+		printf("please input password:");	
+		gets(password);
+	}
+	if(i == 3)
+	{
+		printf("err\n");
+		return;
+	}
+	else
+		printf("input right\n");	
+}
+
+
+
+void main()
+{
+	void user_login();	
+}
