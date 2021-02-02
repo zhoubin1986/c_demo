4f878c3714d95337653241f213a1585e8ea603ca
zhoubin1986 <985619620@qq.com>
2021-02-02 13:45:10 -0300

    divisor_multiple_01.c motified

--- divisor_multiple_01.c
+++ divisor_multiple_01.c
@@ -3,21 +3,21 @@
 void main()
 {
 	int a,b,m,r;
+	int temp;
 	do
 	{
 		printf("please input a b value and a > b >0:");
 		scanf("%d%d",&a,&b);
 	}while(a <= 0 || b <= 0 || a > b);
 	m = a * b;
-	/*
 	do
 	{
 		r = b % a;
+		if(0 == r)
+			temp = a;
 		b = a;
 		a = r;
 	}while(r != 0);
-	*/
-	for()
-	printf("max_divisor = %d\n",a);
-	printf("min_multiple = %d\n",m/a);	//not a*b/r
+	printf("max_divisor = %d\n",temp);
+	printf("min_multiple = %d\n",m/temp);	//not a*b/r
 }
